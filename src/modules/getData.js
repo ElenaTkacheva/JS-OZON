b09497e27f012fcde15d94996403c7e5e2072de3
ElenaTkacheva <79642915+ElenaTkacheva@users.noreply.github.com>
2022-05-18 13:34:34 -0400

    JSONPlaceholder, JSON-Server, Firebase

---
+++ src/modules/getData.js
@@ -0,0 +1,11 @@
+const getData = () => {
+    return fetch(
+      "https://test-c0ab1-default-rtdb.firebaseio.com/goods.json"
+    ).then((response) => {
+        return response.json();
+    });
+
+};
+
+
+export default getData;
